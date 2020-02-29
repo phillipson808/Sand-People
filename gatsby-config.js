2df63f107e9675a7e660a22ae70c8e08118a0883
Spencer Phillipson <phillipson808@gmail.com>
2020-02-28 17:24:10 -0800

    first-commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,41 @@
  */
 
 module.exports = {
-  /* Your site config here */
+plugins: [
+  `gatsby-plugin-sharp`,
+  `gatsby-transformer-sharp`,
+  `gatsby-plugin-sass`,
+  `gatsby-plugin-react-helmet`,
+  {
+    resolve: `gatsby-source-shopify`,
+    options: {
+      // The domain name of your Shopify shop.
+      shopName: `[shopname]`,
+      // The storefront access token
+      accessToken: `[token]`,
+      apiVersion: "2020-01",
+      
+      verbose: true,
+
+      paginationSize: 250,
+
+      includeCollections: ["shop", "content"],
+    },
+  },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `src`,
+      path: `${__dirname}/src/`,
+    },
+  },
+  {
+    resolve: `gatsby-plugin-sharp`,
+    options: {
+      useMozJpeg: false,
+      stripMetadata: true,
+      defaultQuality: 75,
+    },
+  },
+]
 }
