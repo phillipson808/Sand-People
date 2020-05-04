09dab22c81939e475684ef7ff2212290329e64b7
Spencer Phillipson <phillipson808@gmail.com>
2020-05-04 16:44:16 -0700

    Working on locations page

--- src/pages/locations.js
+++ src/pages/locations.js
@@ -2,87 +2,71 @@ import React from "react"
 import Layout from "../components/Layout"
 import styles from "../styles/locations.module.scss"
 import phone from "../img/phone.svg"
+import oahuImage from "../img/moke.jpg"
+import kauaiImage from '../img/kauai.jpg'
+import mauiImage from '../img/maui.jpg'
 
 const locations = () => {
   return (
     <Layout>
-      {/*Showcase*/}
-      <div className={styles.showcase}>
+      {/*Main Showcase*/}
+      <div id={styles.Showcase}>
         <div className={styles.showcaseContent}>
           <h1>Locations</h1>
-          <p>
-            We're proud to offer multiple locations on the islands of Maui,
-            Oahu, and Kauai.
-          </p>
+          <p>View our locations on Kauai, Maui, and Oahu.</p>
         </div>
       </div>
-      {/*Content*/}
 
-      {/*Kauai*/}
-      <div className={styles.locationsContainer}>
-        <div className={styles.kauai}>
-          <div className={styles.kauaiImage}></div>
-          <div className={styles.locationsContent}>
-            <h1>Kauai</h1>
-            <h2>The Shops at Kukuiula</h2>
-            <p>2829 Ala Kalanikaumaka St #G141 Koloa, HI 96756</p>
-            <div className={styles.locationPhone}>
-              <img src={phone} />
-              <p className={styles.phone}>(808) 742-2888</p>
-            </div>
-            <div className={styles.hanalei}>
-              <h2>Hanalei</h2>
-              <p>5-5161 Kuhio Hwy # 108, Hanalei, HI 96714</p>
-              <div className={styles.locationPhone}>
-                <img src={phone} />
-                <p className={styles.phone}>(808) 826-1008</p>
-              </div>
-            </div>
+      {/*Island Containers*/}
+
+      <div className={styles.islandContainer}>
+        <div className={styles.islandContent}>
+          <div className={styles.islandImageContainer}>
+            <img
+              src={kauaiImage}
+              alt="Oahu"
+              className={styles.islandImage}
+            ></img>
           </div>
-        </div>
-      </div>
-      {/*Oahu*/}
-      <div className={styles.locationsContainer}>
-        <div className={styles.kauai}>
-          <div className={styles.oahuImage}></div>
-          <div className={styles.locationsContent}>
-            <h1>Oahu</h1>
-            <h2>The Shops at Kukuiula</h2>
-            <p>2829 Ala Kalanikaumaka St #G141 Koloa, HI 96756</p>
-            <div className={styles.locationPhone}>
-              <img src={phone} />
-              <p className={styles.phone}>(808) 742-2888</p>
-            </div>
-            <div className={styles.hanalei}>
-              <h2>Hanalei</h2>
-              <p>5-5161 Kuhio Hwy # 108, Hanalei, HI 96714</p>
-              <div className={styles.locationPhone}>
-                <img src={phone} />
-                <p className={styles.phone}>(808) 826-1008</p>
+    
+          <div className={styles.islandTextContainer}>
+          <h1>Kauai</h1>
+          <div className={styles.ruler}></div>
+            <div className={styles.islandFlexContainer}>
+              <div className={styles.address}>
+                <h3>The Shops at Kukuiula</h3>
+                <p>2829 Ala Kalanikaumaka St. #G141</p>
+                <div className={styles.phoneContainer}>
+                  <img src={phone}></img>
+                  <p>(808) 826-1008</p>
+                </div>
+                <a href="https://compressor.io/compress" target="_blank">
+                  <button className={styles.directionsbtn}>Directions</button>
+                </a>
+              </div>
+              <div className={styles.address}>
+                <h3>The Shops at Kukuiula</h3>
+                <p>2829 Ala Kalanikaumaka St. #G141</p>
+                <div className={styles.phoneContainer}>
+                  <img src={phone}></img>
+                  <p>(808) 826-1008</p>
+                </div>
+
+                <a href="https://compressor.io/compress" target="_blank">
+                  <button className={styles.directionsbtn}>Directions</button>
+                </a>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
-      {/*Maui*/}
-      <div className={styles.locationsContainer}>
-        <div className={styles.kauai}>
-          <div className={styles.kauaiImage}></div>
-          <div className={styles.locationsContent}>
-            <h1>Maui</h1>
-            <h2>The Shops at Kukuiula</h2>
-            <p>2829 Ala Kalanikaumaka St #G141 Koloa, HI 96756</p>
-            <div className={styles.locationPhone}>
-              <img src={phone} />
-              <p className={styles.phone}>(808) 742-2888</p>
-            </div>
-            <div className={styles.hanalei}>
-              <h2>Hanalei</h2>
-              <p>5-5161 Kuhio Hwy # 108, Hanalei, HI 96714</p>
-              <div className={styles.locationPhone}>
-                <img src={phone} />
-                <p className={styles.phone}>(808) 826-1008</p>
+            <div className={styles.address}>
+              <h3>Hanalei</h3>
+              <p>5-5161 Kuhio Hwy #108, Hanalei, HI 96714</p>
+              <div className={styles.phoneContainer}>
+                <img src={phone}></img>
+                <p>(808) 826-1008</p>
               </div>
+              <a href="https://compressor.io/compress" target="_blank">
+                <button className={styles.directionsbtn}>Directions</button>
+              </a>
             </div>
           </div>
         </div>
