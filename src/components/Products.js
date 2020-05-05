c3e0662e4b79f33d6792cf6396907ddd2062aab1
Spencer Phillipson <phillipson808@gmail.com>
2020-05-05 13:51:12 -0700

    Added showcase to products page

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,6 +2,7 @@ import React from "react"
 import { useStaticQuery, graphql, Link } from "gatsby"
 import styles from "../styles/product.module.scss"
 import Img from "gatsby-image"
+import productImage from "../img/product-display.jpg"
 
 const Products = () => {
   let data = useStaticQuery(graphql`
@@ -37,32 +38,41 @@ const Products = () => {
     }
   `)
   data = data.allShopifyProduct.edges
-  console.log("DATA", data)
+
   return (
-    <div className={styles.container}>
-      <h1 className={styles.pageTitle}>All Products</h1>
-      <div className={styles.grid}>
-        {data.map(item => {
-          return (
-            <div className={styles.productImage} key={item.node.shopifyId}>
-              <Link to={`/products/${item.node.handle}`}><Img
-                className={styles.gridImage}
-                fluid={item.node.images[0].localFile.childImageSharp.fluid}
-              ></Img></Link>
-              <div className={styles.content}>
-                <div>
-                  <h4 className={styles.productDesc}>{item.node.title}</h4>
-                  <p className={styles.productDesc}>
-                    $
-                    {Number(
-                      item.node.priceRange.maxVariantPrice.amount
-                    ).toFixed(2)}
-                  </p>
+    <div>
+      <div id={styles.Showcase}>
+        <div className={styles.showcaseContent}>
+          <h1>All Products</h1>
+          <p>Shop our collection of ocean-inspired products.</p>
+        </div>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.grid}>
+          {data.map(item => {
+            return (
+              <div className={styles.productImage} key={item.node.shopifyId}>
+                <Link to={`/products/${item.node.handle}`}>
+                  <Img
+                    className={styles.gridImage}
+                    fluid={item.node.images[0].localFile.childImageSharp.fluid}
+                  ></Img>
+                </Link>
+                <div className={styles.content}>
+                  <div>
+                    <h4 className={styles.productDesc}>{item.node.title}</h4>
+                    <p className={styles.productDesc}>
+                      $
+                      {Number(
+                        item.node.priceRange.maxVariantPrice.amount
+                      ).toFixed(2)}
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-          )
-        })}
+            )
+          })}
+        </div>
       </div>
     </div>
   )
