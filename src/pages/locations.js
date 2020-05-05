1a679dfc42994960ceb82a47be40f329bcbfc691
Spencer Phillipson <phillipson808@gmail.com>
2020-05-05 09:54:46 -0700

    Working on responsive design for locations page

--- src/pages/locations.js
+++ src/pages/locations.js
@@ -3,8 +3,8 @@ import Layout from "../components/Layout"
 import styles from "../styles/locations.module.scss"
 import phone from "../img/phone.svg"
 import oahuImage from "../img/moke.jpg"
-import kauaiImage from '../img/kauai.jpg'
-import mauiImage from '../img/maui.jpg'
+import kauaiImage from "../img/kauai.jpg"
+import mauiImage from "../img/maui.jpg"
 
 const locations = () => {
   return (
@@ -18,55 +18,85 @@ const locations = () => {
       </div>
 
       {/*Island Containers*/}
-
       <div className={styles.islandContainer}>
         <div className={styles.islandContent}>
           <div className={styles.islandImageContainer}>
             <img
-              src={kauaiImage}
+              src={oahuImage}
               alt="Oahu"
               className={styles.islandImage}
             ></img>
           </div>
-    
+
           <div className={styles.islandTextContainer}>
-          <h1>Kauai</h1>
-          <div className={styles.ruler}></div>
+            <h1>Oahu</h1>
+            <div className={styles.ruler}></div>
             <div className={styles.islandFlexContainer}>
               <div className={styles.address}>
-                <h3>The Shops at Kukuiula</h3>
-                <p>2829 Ala Kalanikaumaka St. #G141</p>
+                <h3>Sand People Kailua</h3>
+                <p>600 Kailua Road #118, Kailua, HI 96734</p>
                 <div className={styles.phoneContainer}>
                   <img src={phone}></img>
-                  <p>(808) 826-1008</p>
+                  <p>(808) 261-8878</p>
                 </div>
                 <a href="https://compressor.io/compress" target="_blank">
                   <button className={styles.directionsbtn}>Directions</button>
                 </a>
               </div>
               <div className={styles.address}>
-                <h3>The Shops at Kukuiula</h3>
-                <p>2829 Ala Kalanikaumaka St. #G141</p>
+                <h3>Sand Kids Kailua</h3>
+                <p>600 Kailua Road #117, Kailua, HI 96734</p>
                 <div className={styles.phoneContainer}>
                   <img src={phone}></img>
-                  <p>(808) 826-1008</p>
+                  <p>(808) 261-4689</p>
                 </div>
 
                 <a href="https://compressor.io/compress" target="_blank">
                   <button className={styles.directionsbtn}>Directions</button>
                 </a>
               </div>
-            </div>
-            <div className={styles.address}>
-              <h3>Hanalei</h3>
-              <p>5-5161 Kuhio Hwy #108, Hanalei, HI 96714</p>
-              <div className={styles.phoneContainer}>
-                <img src={phone}></img>
-                <p>(808) 826-1008</p>
+              <div className={styles.address}>
+                <h3>Sand People Ala Moana</h3>
+                <p>
+                  <i>*Ala Moana Shopping Center</i>
+                </p>
+                <p>
+                  <br /> Ala Moana Blvd, Space 2254 Honolulu, HI 96814
+                </p>
+                <div className={styles.phoneContainer}>
+                  <img src={phone}></img>
+                  <p>(808) 826-1008</p>
+                </div>
+                <a href="https://compressor.io/compress" target="_blank">
+                  <button className={styles.directionsbtn}>Directions</button>
+                </a>
               </div>
-              <a href="https://compressor.io/compress" target="_blank">
-                <button className={styles.directionsbtn}>Directions</button>
-              </a>
+
+              <div className={styles.address}>
+                <h3>Sand People Haleiwa</h3>
+                <p>66-249 Kamehameha Hwy, Haleiwa, HI 96712</p>
+                <div className={styles.phoneContainer}>
+                  <img src={phone}></img>
+                  <p>(808) 637-3373</p>
+                </div>
+                <a href="https://compressor.io/compress" target="_blank">
+                  <button className={styles.directionsbtn}>Directions</button>
+                </a>
+              </div>
+
+              <div className={styles.address}>
+                <h3>Sand People Int'l Marketplace</h3>
+                <p><i>*International Marketplace</i></p>
+                <br/>
+                <p>2330 Kalakaua Avenue #207, Honolulu, HI 96815</p>
+                <div className={styles.phoneContainer}>
+                  <img src={phone}></img>
+                  <p>(808) 800-3314</p>
+                </div>
+                <a href="https://compressor.io/compress" target="_blank">
+                  <button className={styles.directionsbtn}>Directions</button>
+                </a>
+              </div>
             </div>
           </div>
         </div>
