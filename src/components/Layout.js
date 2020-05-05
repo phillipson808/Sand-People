4c0b3562b6e874b690f514ec601cbefe85afeedf
Spencer Phillipson <phillipson808@gmail.com>
2020-05-05 14:48:13 -0700

    Fixed footer so it stays at bottom of page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,17 @@
 import React from "react"
 import Head from "./Head"
-import Nav from './Nav'
-import Footer from './Footer'
+import Nav from "./Nav"
+import Footer from "./Footer"
+import styles from "../styles/layout.module.scss"
 
 const Layout = props => {
   return (
-      <div>
-        <Head />
-        <Nav />
-        {props.children}
-        <Footer />
-      </div>
+    <div className={styles.contentContainer}>
+      <Head />
+      <Nav />
+      <div className={styles.contentContainer}>{props.children}</div>
+      <Footer />
+    </div>
   )
 }
 
