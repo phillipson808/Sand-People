e88d2f6aaa2db8397f25a7028d102dc04aa1aa3f
Spencer Phillipson <phillipson808@gmail.com>
2020-05-13 17:06:35 -0700

    Fixing path error on master

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,13 +1,11 @@
 import React from "react"
 import { Helmet } from "react-helmet"
-import ShopifyScript from "../../public/static/ShopifyScript.js"
 
 
 const Head = () => {
   return (
     <Helmet>
       <title>Sand People</title>
-      <script src={ShopifyScript}></script>
     </Helmet>
   )
 }
