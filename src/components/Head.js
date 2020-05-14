2051d3c3be98898d734c6e7842e72ed7b950ec0d
Spencer Phillipson <phillipson808@gmail.com>
2020-05-14 14:16:55 -0700

    Added favicon

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,11 +1,14 @@
 import React from "react"
 import { Helmet } from "react-helmet"
+import favicon from '../img/favicon.ico'
 
 
 const Head = () => {
   return (
     <Helmet>
       <title>Sand People</title>
+      <link rel="icon" href={favicon} />
+
     </Helmet>
   )
 }
