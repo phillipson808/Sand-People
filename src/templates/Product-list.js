841a0025a81deb480166c510415f100f34c0d256
phillipson808 <46663631+phillipson808@users.noreply.github.com>
2022-06-30 17:35:27 -0700

    Update Product-list.js

--- src/templates/Product-list.js
+++ src/templates/Product-list.js
@@ -22,7 +22,7 @@ export const query = graphql`
             localFile {
               childImageSharp {
                 fluid(quality: 100, maxWidth: 500) {
-                  ...GatsbyImageSharpFluid_tracedSVG
+                  ...GatsbyImageSharpFluid
                 }
               }
             }
